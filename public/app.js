e45535f3409a7041dac449c2bcdf439bac136731
zavianmali23.com <z.avianmali2020@gmail.com>
2024-01-22 10:06:16 +0300

    Typescript Config

---
+++ public/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let age;
+age = true;
+console.log(age);
+age = 30;
+console.log(age);
+age = { name: 'MM', age: 30 };
+console.log(age);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+var age;
+age = true;
+console.log(age);
+age = 30;
+console.log(age);
+age = { name: 'MM', age: 30 };
+console.log(age);
