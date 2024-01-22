a30e5e5559d61c9986ee1c78aae0005a060a70c5
zavianmali23.com <z.avianmali2020@gmail.com>
2024-01-22 07:15:51 +0300

    Objects and Arrays

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-var first_name = 'Johnson';
-var age = 30;
-var isDope = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+var games = ['Mk1', 'COD', 'GTA'];
+games.push('NFS');
+console.log(games);
+// nymber types
+var numbers = [1, 2, 3];
+numbers.push(10);
+console.log(numbers);
+// mixed variable types
+var mixed = ['Hitman', 3, 'SF', 9];
+mixed.push('halo');
+console.log(mixed);
+// objects
+var user = {
+    fname: 'K',
+    age: 30
 };
-var newDi = circ(20);
-console.log(newDi);
+// user.skills = ['joa', 'fi']
+console.log(user);
--- app.ts
+++ app.ts
@@ -1,10 +1,34 @@
-let first_name = 'Johnson';
-let age = 30;
-let isDope = true;
+// arrays
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
-}
+let games = ['Mk1', 'COD', 'GTA'];
 
-const newDi = circ(20);
-console.log(newDi)
+games.push('NFS');
+
+console.log(games);
+// nymber types
+
+let numbers = [1, 2, 3]
+
+numbers.push(10);
+
+console.log(numbers);
+
+// mixed variable types
+
+let mixed = ['Hitman', 3, 'SF', 9]
+
+mixed.push('halo')
+
+console.log(mixed);
+
+
+// objects
+
+let user = {
+  fname: 'K',
+  age: 30,
+};
+
+// user.skills = ['joa', 'fi']
+
+console.log(user)
