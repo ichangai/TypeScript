76eafd77a3c0c61ac8fb81f53ab04e02556f37cf
zavianmali23.com <z.avianmali2020@gmail.com>
2024-01-23 00:12:39 +0300

    Classes and Access Modifiers

--- public/app.js
+++ public/app.js
@@ -1,22 +1,49 @@
 "use strict";
-// example
-let greet;
-greet = (name, greeting) => {
-    console.log(`${name} says ${greeting}`);
-};
-// greet('jay', 'chao adios')
-// example 2
-let calc;
-calc = (numOne, numTwo, action) => {
-    if (action === '+') {
-        return numOne + numTwo;
+// class Invoice {
+//   readonly client: string;
+//   private details: string;
+//   private amount: number
+//   constructor(c: string, d: string, a: number) {
+//     this.client = c;
+//     this.details = d;
+//     this.amount = a;
+//   }
+//   format() {
+//     return `${this.client} owes ${this.amount} for ${this.details}`
+//   }
+// }
+class Invoice {
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
-    else {
-        return numOne - numTwo;
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
     }
-};
-let logDetails;
-logDetails = (ninja) => {
-    console.log(`${ninja.name} is ${ninja.age}`);
-};
-logDetails({ name: 'icha', age: 30 });
+}
+const invOne = new Invoice('Ichan', 'work on website', 240);
+const invTwo = new Invoice('Jackk', 'work on website', 400);
+// console.log(invOne, invTwo)
+invOne.client = 'Mike';
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+const anchor = document.querySelector('a');
+if (anchor) {
+    // console.log(anchor.href);
+}
+// console.log(anchor.href);
+//const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form');
+// console.log(form.children);
+// inputs
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,70 @@
-// example
-let greet: (a: string, b: string) => void;
-greet = (name: string, greeting: string) => {
-  console.log(`${name} says ${greeting}`)
-}
+// class Invoice {
+//   readonly client: string;
+//   private details: string;
+//   private amount: number
 
-// greet('jay', 'chao adios')
+//   constructor(c: string, d: string, a: number) {
+//     this.client = c;
+//     this.details = d;
+//     this.amount = a;
+//   }
 
+//   format() {
+//     return `${this.client} owes ${this.amount} for ${this.details}`
+//   }
+// }
 
-// example 2
-let calc: (a: number, b: number, c: string) => number;
+class Invoice {
+  
+ 
 
-calc = (numOne: number, numTwo: number, action: string) => {
-  if (action === '+') {
-    return numOne + numTwo;
-  } else {
-    return numOne - numTwo;
+  constructor(
+    readonly client: string,
+    private details: string,
+    private amount: number
+  ) {
+  }
+
+  format() {
+    return `${this.client} owes ${this.amount} for ${this.details}`
   }
 }
 
-// example 3
+const invOne = new Invoice('Ichan', 'work on website', 240)
+const invTwo = new Invoice('Jackk', 'work on website', 400)
 
-type myObject = { name: string, age: number }
+// console.log(invOne, invTwo)
+invOne.client = 'Mike'
 
-let logDetails: (obj: myObject) => void;
+let invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
 
-logDetails = (ninja: myObject) => {
-  console.log(`${ninja.name} is ${ninja.age}`)
+console.log(invoices);
+
+const anchor = document.querySelector('a')!;
+if (anchor) {
+  // console.log(anchor.href);
 }
+// console.log(anchor.href);
 
-logDetails({name:'icha', age: 30})
+//const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children);
+
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value,
+    tofrom.value,
+    details.value,
+    amount.valueAsNumber
+  );
+});
