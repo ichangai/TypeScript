380824ae2f58d88b0a941674eaf2705db920515f
zavianmali23.com <z.avianmali2020@gmail.com>
2024-01-22 06:48:33 +0300

    Typescript

---
+++ app.js
@@ -0,0 +1,8 @@
+var first_name = 'Johnson';
+var age = 30;
+var isDope = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+var newDi = circ(20);
+console.log(newDi);
---
+++ app.ts
@@ -0,0 +1,10 @@
+let first_name = 'Johnson';
+let age = 30;
+let isDope = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+}
+
+const newDi = circ(20);
+console.log(newDi)
